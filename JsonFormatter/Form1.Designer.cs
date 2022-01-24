9f9ca02f5fc5cd6d0ca4c5bc2e9b1511bae91bfd
Vyacheslav <slava021323@gmail.com>
2022-01-25 03:01:00 +0600

    1.0.0

---
+++ JsonFormatter/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace JsonFormatter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.InputGroupBox = new System.Windows.Forms.GroupBox();
+            this.InputTextBox = new System.Windows.Forms.RichTextBox();
+            this.OutputGroupBox = new System.Windows.Forms.GroupBox();
+            this.CopyOutputButton = new System.Windows.Forms.Button();
+            this.OutputTextBox = new System.Windows.Forms.RichTextBox();
+            this.InputGroupBox.SuspendLayout();
+            this.OutputGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InputGroupBox
+            // 
+            this.InputGroupBox.Controls.Add(this.InputTextBox);
+            this.InputGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InputGroupBox.Location = new System.Drawing.Point(12, 7);
+            this.InputGroupBox.Name = "InputGroupBox";
+            this.InputGroupBox.Size = new System.Drawing.Size(374, 545);
+            this.InputGroupBox.TabIndex = 0;
+            this.InputGroupBox.TabStop = false;
+            this.InputGroupBox.Text = "Input";
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InputTextBox.Location = new System.Drawing.Point(6, 23);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(362, 516);
+            this.InputTextBox.TabIndex = 0;
+            this.InputTextBox.Text = "";
+            this.InputTextBox.TextChanged += new System.EventHandler(this.InputTextBox_TextChanged);
+            // 
+            // OutputGroupBox
+            // 
+            this.OutputGroupBox.Controls.Add(this.CopyOutputButton);
+            this.OutputGroupBox.Controls.Add(this.OutputTextBox);
+            this.OutputGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OutputGroupBox.Location = new System.Drawing.Point(396, 7);
+            this.OutputGroupBox.Name = "OutputGroupBox";
+            this.OutputGroupBox.Size = new System.Drawing.Size(374, 545);
+            this.OutputGroupBox.TabIndex = 1;
+            this.OutputGroupBox.TabStop = false;
+            this.OutputGroupBox.Text = "Output";
+            // 
+            // CopyOutputButton
+            // 
+            this.CopyOutputButton.Enabled = false;
+            this.CopyOutputButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CopyOutputButton.Location = new System.Drawing.Point(6, 508);
+            this.CopyOutputButton.Name = "CopyOutputButton";
+            this.CopyOutputButton.Size = new System.Drawing.Size(362, 31);
+            this.CopyOutputButton.TabIndex = 2;
+            this.CopyOutputButton.Text = "Copy to Clipboard";
+            this.CopyOutputButton.UseVisualStyleBackColor = true;
+            this.CopyOutputButton.Click += new System.EventHandler(this.CopyOutputButton_Click);
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OutputTextBox.Location = new System.Drawing.Point(6, 23);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.ReadOnly = true;
+            this.OutputTextBox.Size = new System.Drawing.Size(362, 479);
+            this.OutputTextBox.TabIndex = 1;
+            this.OutputTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(782, 563);
+            this.Controls.Add(this.OutputGroupBox);
+            this.Controls.Add(this.InputGroupBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JSON Formatter";
+            this.InputGroupBox.ResumeLayout(false);
+            this.OutputGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox InputGroupBox;
+        private System.Windows.Forms.GroupBox OutputGroupBox;
+        private System.Windows.Forms.RichTextBox InputTextBox;
+        private System.Windows.Forms.RichTextBox OutputTextBox;
+        private System.Windows.Forms.Button CopyOutputButton;
+    }
+}
+
